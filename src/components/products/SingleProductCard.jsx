114008d212867046c9f210b212adff5154b21885
rokas <rokasra@gmail.com>
2022-06-10 14:40:38 +0300

    json api

--- src/components/products/SingleProductCard.jsx
+++ src/components/products/SingleProductCard.jsx
@@ -5,8 +5,8 @@ const SingleProductCard = (props) => {
     <div className='product-card'>
       <img className='product-image' src={props.image} alt={props.title} />
       <h3>{props.title}</h3>
-      <p>{props.price}</p>
-      <Link to={`products/${props.id}`}>
+      <p>$ {props.price}</p>
+      <Link to={`/products/${props.id}`}>
         <button>Read more</button>
       </Link>
     </div>
--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -1,10 +1,38 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+
+// parissiusti ir issaugoti state produkto kurio id === productId
+// duomenis ir supildyti jsx
+
 const SingleProductPage = () => {
+  const [currentProduct, setCurrentProduct] = useState({});
+
+  const { productId } = useParams();
+
+  const getCurrentProductAndSave = async () => {
+    const resp = await fetch('/db/products.json');
+    const dataInJs = await resp.json();
+    console.log('dataInJs ===', dataInJs);
+    console.log('productId ===', productId);
+    const currentProduct = dataInJs.find(({ id }) => id === +productId);
+    console.log('currentProduct ===', currentProduct);
+    setCurrentProduct(currentProduct);
+  };
+
+  useEffect(() => {
+    getCurrentProductAndSave();
+  }, []);
+
   return (
     <div>
-      <h1>Title</h1>
-      <h2>price</h2>
-      <img src='' alt='image' />
-      <p className='desc'>description</p>
+      <h1>{currentProduct.title}</h1>
+      <h2>{currentProduct.price}</h2>
+      <img src={currentProduct.image} alt='image' />
+      <p className='description'>{currentProduct.description} </p>
+      <Link to='/products'>
+        <button>Go back</button>
+      </Link>
     </div>
   );
 };
